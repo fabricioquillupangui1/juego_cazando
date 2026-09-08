669e6a0e9cd9d6ab324c92400c34ba7fa3320a96
fabricioquillupangui1 <fquillupangui19@gmail.com>
2026-09-07 19:20:12 -0500

    agregar funcion restarTiempo,variables,condiciones de juego y usar funciones de utils.js

--- cazando.js
+++ cazando.js
@@ -9,6 +9,8 @@ let gatoY = 0;
 let comidaX = 0;
 let comidaY = 0; 
 let puntaje = 0;
+let tiempo = 10;
+let intervalo;
 function graficarGato() {
     graficarRectangulo(gatoX, gatoY, ANCHO_GATO, ALTURA_GATO, "#300");
 }
@@ -18,9 +20,13 @@ function graficarComida() {
 }
 
 function iniciarJuego() {
+    if(intervalo){clearInterval(intervalo);}
+    intervalo=setInterval(restarTiempo,1000);
     gatoX = (canvas.width / 2) - (ANCHO_GATO / 2);
     gatoY = (canvas.height / 2) - (ALTURA_GATO / 2);
     reaparecerComida();
+    mostrarEnSpan("tiempo",tiempo);
+    mostrarEnSpan("puntos",puntaje);
     actualizarPantalla();
 }
 
@@ -67,9 +73,19 @@ function detectarColision (){
     //alert("ATRAPADO!!");
     puntaje = puntaje +1;
     mostrarEnSpan("puntos",puntaje);
+    if(puntaje==6){clearInterval(intervalo);
+        alert("!GANADOR!");}
     reaparecerComida();
     actualizarPantalla();
     }
+}
+
+function restarTiempo(){
+tiempo = tiempo -1;
+mostrarEnSpan("tiempo",tiempo);
+
+    if(tiempo<=0){clearInterval(intervalo);
+        alert("GAME OVER");}
 }
 
 function actualizarPantalla(){
